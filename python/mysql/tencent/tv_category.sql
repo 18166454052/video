dcf31a7d39a95e972d6f5ed9a680cb9d612e3e5f
18166454052 <2198178942@qq.com>
2019-07-13 13:33:54 +0800

    删除python

--- python/mysql/tencent/tv_category.sql
+++
@@ -1,8 +0,0 @@
-create table `tv_category` (
-     `id`  int NOT NULL  PRIMARY KEY  AUTO_INCREMENT,
-     `name` varchar(20) NOT NULL,
-     `url` varchar(50) NOT NULL,
-     `type` varchar(30) NOT NULL,
-     `type_val` varchar(50) NOT NULL,
-     `label` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
--- python/mysql/tencent/variety_category.sql
+++
@@ -1,8 +0,0 @@
-create table `variety_category` (
-     `id`  int NOT NULL  PRIMARY KEY  AUTO_INCREMENT,
-     `name` varchar(20) NOT NULL,
-     `url` varchar(50) NOT NULL,
-     `key` varchar(30) NOT NULL,
-     `key_val` varchar(50) NOT NULL,
-     `label` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
